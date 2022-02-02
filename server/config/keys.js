4fdc251e92a7538844fe034f58cd1e85f890aafd
Zenith Anthony <dabarerawza.19@uom.lk>
2022-02-02 22:03:43 +0530

    added

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://zenith:Zenith1997@cluster0.gte9b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'//dulnaths'
+  
+    //  mongoURI: 'mongodb+srv://Kumuthu:omega123@cluster0.ehhq0.mongodb.net/influencer_hub?retryWrites=true&w=majority'//dulnath
+   // mongoURI: 'mongodb+srv://auth:Zenith124@authmern.8wpst.mongodb.net/infuencer_hub?retryWrites=true&w=majority'//zenith
+  // mongodb+srv://amasha:<by5x23mw>@cluster0.kilgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority//amasha
+
+}
